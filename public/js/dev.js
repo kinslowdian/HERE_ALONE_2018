7338f9a7604cd5ddd40385d5b2d8df42a5066f67
Kinslow <simon.kinslow@aa7778.weu.grey.global>
2018-07-09 09:13:20 +0100

    Extra movement on intro character

--- public/js/dev.js
+++ public/js/dev.js
@@ -35,6 +35,13 @@ function intro_init()
 	displayList.introBtn = displayList_add(".ha-intro-btn");
 	displayList.introBtnMain = displayList_add(".ha-intro-btn p");
 
+	displayList.introSadly = displayList_add(".ha-intro-top .sadly");
+	displayList.introSadlyBody = displayList_add(".ha-intro-top .sadly-top");
+	displayList.introSadlyLeg0 = displayList_add(".ha-intro-top .sadly-leg0");
+	displayList.introSadlyLeg1 = displayList_add(".ha-intro-top .sadly-leg1");
+	displayList.introSadlyJawT = displayList_add(".ha-intro-top .sadly-jawT");
+	displayList.introSadlyJawB = displayList_add(".ha-intro-top .sadly-jawB");
+
 	displayList.mute = displayList_add(".mute");
 }
 
@@ -55,6 +62,8 @@ function intro_ready()
 
 function intro_event(event)
 {
+	let delay;
+
 	if(system.touchDevice)
 	{
 		displayList.introBtnMain.removeEventListener("touchend", intro_event, false);
@@ -69,7 +78,15 @@ function intro_event(event)
 		intro_soundTest();
 	}
 
-	intro_remove();
+	// intro_remove();	
+
+	displayList.introSadlyJawT.classList.add("setting-jaw-2");
+	displayList.introSadlyJawB.classList.add("setting-jaw-2");
+	displayList.introSadlyBody.classList.add("tween-walk-body");
+	displayList.introSadlyLeg0.classList.add("tween-walk-leg-0");
+	displayList.introSadlyLeg1.classList.add("tween-walk-leg-1");
+
+	delay = setTimeout(intro_remove, 3 * 1000);
 
 	hack_levelLoad();
 }
@@ -77,6 +94,10 @@ function intro_event(event)
 function intro_remove()
 {
 	displayList.intro.remove();
+
+	delete displayList.introSadly;
+	delete displayList.introSadlyJawT;
+	delete displayList.introSadlyJawB;
 }
 
 
